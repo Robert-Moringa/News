798f89882b60a3b4acaac0eea6a31a239257388c
Robert Maina <robert.maina@student.moringaschool.com>
2021-08-08 11:56:44 +0300

    used macro.html to define sources

--- app/views.py
+++ app/views.py
@@ -9,10 +9,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
+    general = getNews_sources('general')
     business = getNews_sources('business')
     sports = getNews_sources('sports')
     tech = getNews_sources('technology')
     entertainment = getNews_sources('entertainment')
+    science = getNews_sources('science')
+    health = getNews_sources('health')
+    
 
-    return render_template('index.html', business = business, sports = sports, tech = tech, entertainment = entertainment )
+
+    return render_template('index.html', health = health, health = health, business = business, sports = sports, tech = tech, entertainment = entertainment , general = general)
