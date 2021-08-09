48e47833207ee4d5b87ee41259291efb43a65d8d
Robert Maina <robert.maina@student.moringaschool.com>
2021-08-09 16:33:26 +0300

    added styles block on articles.html and styled it

--- app/views.py
+++ app/views.py
@@ -16,16 +16,19 @@ def index():
     entertainment = getNews_sources('entertainment')
     science = getNews_sources('science')
     health = getNews_sources('health')
+
+    title= 'Welcome to the best News Website ever!'
     
     print(general)
 
-    return render_template('index.html', health = health, science = science, business = business, sports = sports, tech = tech, entertainment = entertainment , general = general)
+    return render_template('index.html', title=title, health = health, science = science, business = business, sports = sports, tech = tech, entertainment = entertainment , general = general)
 
 @app.route('/articles/<id>')
 def articles(id):
 	'''
 	view articles page
 	'''
+    
 	articles = get_articles(id)
 
 	return render_template('articles.html', articles = articles)
